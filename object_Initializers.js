5a939d07a2c890d6096cdd1a433649f26529becb
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-26 20:43:28 +0530

    Create object_Initializers.js

---
+++ object_Initializers.js
@@ -0,0 +1,11 @@
+var John = {name: "John", age: 25};
+var James = {name: "James", age: 21};
+var Mapogo_coalliation = {
+    alpha : "Makulu", 
+    attacker : "Mr.T"
+}
+
+
+
+document.write(Mapogo_coalliation.alpha);
+
