23af9fc7ea79edb73b178675c212749be529999e
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-27 20:58:36 +0530

    Create JS_Arrays.js

---
+++ JS_Arrays.js
@@ -0,0 +1,11 @@
+var cars = new Array ("Lamborghini", "BMW", "Benz", "Porche")
+
+document.write(cars)
+cars[3] = "NISSAN"
+document.write("<hr>", cars)
+
+
+/*JavaScript Arrays
+
+Arrays store multiple values in a single variable. 
+To store three course names, you need three variables.*/
