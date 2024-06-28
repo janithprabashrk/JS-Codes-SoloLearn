d5bde416da45395777c6d509c5e4c28f5f5e487a
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-28 10:15:07 +0530

    Create Array_Declaring_Types.js

---
+++ Array_Declaring_Types.js
@@ -0,0 +1,8 @@
+var Mapogos = new Array(6);
+Mapogos[0] = "Makhulu - The BOSS";
+Mapogos[1] = "Kinky Tail";
+Mapogos[2] = "Mr. T";
+Mapogos[3] = "Pretty Boy";
+Mapogos[4] = "Scar";
+Mapogos[5] = "Dreadlock";
+document.write("<hr>", "<h1>Mapogos - The Mightiest Warrios of Sabi Sand </h1>",Mapogos);
