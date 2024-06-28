db3dfcaedd092d6dd78255aed770070e60bf4172
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-28 10:15:10 +0530

    Create Array_Literal.js

---
+++ Array_Literal.js
@@ -0,0 +1,5 @@
+/*For greater simplicity, readability, and execution speed, you can also declare arrays using the array literal syntax.*/
+
+var Movies = ["titanic", "avatar", "inception", "interstellar", "Jurassic World", "The Dark Knight", "The Avengers", "The Lion King", "The Hunger Games", "The Lord of the Rings"];
+
+document.write("<hr>", "<h1>Top 10 Movies of All Time</h1>", Movies);
