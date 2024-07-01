793bc382cb08fa786b987f7415aebe4c83c90968
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-07-01 21:06:12 +0530

    Update setInterval.js

--- setInterval.js
+++ setInterval.js
@@ -4,5 +4,5 @@ function myAlert() {
 setInterval(myAlert, 3000);
 
 /*The setInterval() method calls a function or evaluates an expression at specified intervals (in milliseconds).
-
-It will continue calling the function until clearInterval() is called or the window is closed.*/
+It will continue calling the function until clearInterval() is called or the window is closed.
+This will call the <b>myAlert</b> function every 3 seconds (1000 ms = 1 second).*/
