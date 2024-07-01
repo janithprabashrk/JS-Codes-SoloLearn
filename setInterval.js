344c15c94c24c0a78d00df4e4704c3193ce60dbf
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-07-01 20:31:07 +0530

    Create setInterval.js

---
+++ setInterval.js
@@ -0,0 +1,8 @@
+function myAlert() {
+    alert("Hi");
+}
+setInterval(myAlert, 3000);
+
+/*The setInterval() method calls a function or evaluates an expression at specified intervals (in milliseconds).
+
+It will continue calling the function until clearInterval() is called or the window is closed.*/
