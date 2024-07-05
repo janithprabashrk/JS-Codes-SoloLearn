b8acc2f0842852964590b457dcf6c281fa746f2a
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-07-05 20:36:17 +0530

    Create Remove_child.js

---
+++ Remove_child.js
@@ -0,0 +1,8 @@
+// Select the parent element
+const parent = document.getElementById('parentElementId');
+
+// Select the child element to be removed
+const child = document.getElementById('childElementId');
+
+// Remove the child element
+parent.removeChild(child);
