d7ffd8e0a2559af73026008f7ab28fb44e0ec3b6
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-07-05 20:36:23 +0530

    Create Replacing_In_JS.js

---
+++ Replacing_In_JS.js
@@ -0,0 +1,10 @@
+//calling the function in window.onload to make sure the HTML is loaded
+window.onload = function() {
+    var p = document.createElement("p");
+    var node = document.createTextNode("This is new");
+    p.appendChild(node);
+
+    var parent = document.getElementById("demo");
+    var child = document.getElementById("p1");
+    parent.replaceChild(p, child);
+};
